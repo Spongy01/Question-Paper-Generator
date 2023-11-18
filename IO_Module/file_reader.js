7402b4f8f213b7e0e972959f2832e4c6bf4deefe
AliAsgar Padaria <aliasgarpadaria002@gmail.com>
2023-11-18 13:24:32 +0530

    Created the File Reader Module to read questions, added a set of predefined questions into question bank, added comments and JSDoc to file_reader and utils.js

---
+++ IO_Module/file_reader.js
@@ -0,0 +1,66 @@
+const fs = require("node:fs")
+const path = require("node:path")
+const utils = require("../utils")
+
+console.log("------------------------------------------")
+console.log("Current Dir for File Reader Module: ",__dirname)
+console.log("Utils : ", utils)
+console.log("------------------------------------------")
+
+
+/**
+ * @description This function reads the question data from the files specified in 'data_locator.js'
+ * @returns { number,Array } An object containing return code and question data. 
+ */
+
+function read_questions(){
+
+    // Get the locations of data_locations.json file <It holds the file names where data is available for fetching>
+    dataLocationsPath = path.join(utils.baseDir, utils.dataLocator)
+    totalQuestionData = []
+
+    // default condition : All Data read successfully.
+    responseCode = utils.DataReadCodes.FULL_DATA_READ
+
+    try{
+    
+        dataFilesRaw = fs.readFileSync(dataLocationsPath, "utf-8")
+        dataFilesUsable = JSON.parse(dataFilesRaw)
+        locations = dataFilesUsable["locations"]
+        
+        noOfFiles = locations.length
+        dataDir = utils.dataDir
+        console.log("Data Locator found ", noOfFiles, " files containing question data")
+
+        locations.forEach((element,index) => {
+            console.log("Reading File ", index+1, "/",noOfFiles, " | File : ", element)
+        
+            try{
+                filePath = path.join(dataDir, element) 
+                questionDataRaw = fs.readFileSync(filePath,"utf-8")
+                questionDataParsed = JSON.parse(questionDataRaw)
+
+                // merge all questions into a single array.
+                totalQuestionData = [...totalQuestionData, ...questionDataParsed]
+        
+            }catch(fileError){
+                console.error("Caught Error while Reading Data File : ", fileError);
+
+                // If any error during file reading of a particular file, convert responseCode to partial read condition. 
+                responseCode = utils.DataReadCodes.PARTIAL_DATA_READ
+            }
+        });
+
+    }catch (error){
+        // Will reach here if there is some error in reading the contents of data_locations.json file
+        console.error("Caught Error while Reading Data Locator File : ", error);
+        // return with response code : No data read, and an empty array of questions
+        return {code: utils.DataReadCodes.NO_DATA_READ, data : totalQuestionData}
+    }
+
+    return {code: responseCode, data : totalQuestionData}
+
+}
+
+
+module.exports = read_questions
---
+++ utils.js
@@ -0,0 +1,52 @@
+const path = require("node:path")
+
+// Change here if any changes to file systems.
+
+/**
+ * Base directory where the script is located.
+ * @constant {string}
+ */
+const baseDir = __dirname
+
+/**
+ * The name of the folder where data is stored.
+ * Change this if the data folder's name is different.
+ * @constant {string}
+ */
+const dataFolder = "data"
+
+/**
+ * The filename used to locate data locations.
+ * Update this if you want to add more files to read data from
+ * Note: these files should be present in the folder 'dataFolder' to be accessible
+ * @constant {string}
+ */
+const dataLocator = "data_locations.json"
+
+/**
+ * The absolute path to the data directory.
+ * @constant {string}
+ */
+const dataDir = path.join(baseDir, dataFolder)
+
+/**
+ * Constants holding codes for data reading.
+ * @readonly
+ * @enum {number}
+ */
+const DataReadCodes = {
+    /** No data read. */
+    NO_DATA_READ: 0,
+    /** Partial data read. */
+    PARTIAL_DATA_READ: 1,
+    /** Full data read. */
+    FULL_DATA_READ: 2,
+};
+
+
+module.exports.baseDir = baseDir
+module.exports.dataDir = dataDir
+module.exports.dataLocator = dataLocator
+
+module.exports.DataReadCodes = DataReadCodes
+
